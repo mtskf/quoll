6d4391a31a857bb6884ab279e17e31abc6af5e87
Mitsuki Fukunaga <mtskf@users.noreply.github.com>
2026-07-10 07:59:51 +1000

    Revert "feat(webview): sticky current-section heading (#140)" (#141)
    This reverts commit 0ed8f0b7155436a3697bd807a8eae57b40aac1fa.

--- src/webview/cm/sticky-heading/index.ts
+++
@@ -1,3 +0,0 @@
-export { activeStickyHeading } from "./active-heading.js";
-export { quollStickyHeading, stickyHeadingPlugin } from "./sticky-heading-panel.js";
-export { stickyTopHeight } from "./viewport-top.js";
