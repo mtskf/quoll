0eeda5ccde035d7890f4d3dae39971ed5e28e13b
Mitsuki Fukunaga <mitsukifukunaga@gmail.com>
2026-07-09 22:33:13 +1000

    feat(webview): sticky current-section heading ViewPlugin

---
+++ src/webview/cm/sticky-heading/index.ts
@@ -0,0 +1,3 @@
+export { activeStickyHeading } from "./active-heading.js";
+export { quollStickyHeading, stickyHeadingPlugin } from "./sticky-heading-panel.js";
+export { stickyTopHeight } from "./viewport-top.js";
